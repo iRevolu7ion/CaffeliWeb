b0dccd4a701ff7d93b4210a853105f3cf5d470cc
Juan Holguín <irevolu7ion512@gmail.com>
2026-09-05 20:29:58 -0600

    chore: ignore Vercel build output

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-DQeDPXxg.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/Pagina web Caffeli 2.0/Caffeli.com.mx/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-pa5z14WE.js"] }, "/": { filePath: "D:/Pagina web Caffeli 2.0/Caffeli.com.mx/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-Bo2p9kv9.js"] } }, clientEntry: "/assets/index-pa5z14WE.js" });
-export {
-  tsrStartManifest
-};
